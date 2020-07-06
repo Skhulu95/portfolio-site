9c4a0d8a1fb3ee75175aa3ff1ce121152620f074
Thabiso Klip <thebzklip0@gmail.com>
2020-07-06 11:57:59 +0200

    Responsive form

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,8 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
+import MyForm from "./myform.js";
+
 // Set Initial State Of Menu
 let showMenu = false;
 
